7b6723e737b932949d6d8f323d12cb9d560fd13b
creatortime <imaster56@mail.ru>
2025-12-20 02:35:03 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 import CreatorTokenABI from '../contracts/CreatorTokenABI.json';
 
-const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 export default function Home() {
   const [walletAddress, setWalletAddress] = useState('');
@@ -39,7 +39,7 @@ export default function Home() {
   // 3. Регистрация создателя (упрощенно)
   const registerCreator = async () => {
     const provider = new ethers.BrowserProvider(window.ethereum);
-    const signer = provider.getSigner();
+    const signer = await provider.getSigner();
     const contract = new ethers.Contract(CONTRACT_ADDRESS, CreatorTokenABI, signer);
     try {
       const tx = await contract.registerCreator("youtube", "my_channel");
